26d91103baa5f53ba60343c144b6543d4ed3bc14
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-14 06:00:06 -0500

    scraper console logs y-combinator test site: WORKING. does not put in db yet.

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -7,46 +7,44 @@ module.exports = (app) => {
         res.send(`API ROUTE TEST`);
     });
 
-    // A GET route for scraping the echojs website
+    // Scrape data from one site and place it into the mongodb db
     app.get("/scrape", function (req, res) {
-        // First, we grab the body of the html with request
+        // Make a request for the news section of ycombinator
         request("https://news.ycombinator.com/", function (error, response, html) {
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            // Load the html body from request into cheerio
             var $ = cheerio.load(html);
-
-
-            // Now, we grab every h2 within an article tag, and do the following:
-            $("article h2").each(function (i, element) {
-                // Save an empty result object
-                var result = {};
-
-
-                // Add the text and href of every link, and save them as properties of the result object
-                result.title = $(this).children("a").text();
-                result.link = $(this).children("a").attr("href");
+            // For each element with a "title" class
+            $(".title").each(function (i, element) {
+                // Save the text and href of each link enclosed in the current element
+                var title = $(element).children("a").text();
+                var link = $(element).children("a").attr("href");
 
                 // If this found element had both a title and a link
                 if (title && link) {
-                    // Insert the data in the scrapedData db
-                    db.scrapedData.insert({
-                            title: title,
-                            link: link
-                        },
-                        function (err, inserted) {
-                            if (err) {
-                                // Log the error if one is encountered during the query
-                                console.log(err);
-                            } else {
-                                // Otherwise, log the inserted data
-                                console.log(inserted);
-                            }
-                        });
-                    
+                    //     // Insert the data in the scrapedData db
+                    //     db.scrapedData.insert({
+                    //             title: title,
+                    //             link: link
+                    //         },
+                    //         function (err, inserted) {
+                    //             if (err) {
+                    //                 // Log the error if one is encountered during the query
+                    //                 console.log(err);
+                    //             } else {
+                    //                 // Otherwise, log the inserted data
+                    //                 console.log(inserted);
+                    //             }
+                    //         });
+
+                    console.log(`TITLES: ${title}`);
+                    console.log(`LINKS: ${link}`);
                 }
+
+
             });
         });
+
+
+
     });
-
-
-
 };
