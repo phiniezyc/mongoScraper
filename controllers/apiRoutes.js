38d7fe3f1285d08aaf1bd631c99822ecfbe52f17
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-14 05:15:27 -0500

    added scrape route: NOT WORKING

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,7 +1,43 @@
+var request = require('request');
+const cheerio = require("cheerio");
+
 module.exports = (app) => {
     app.get("/api/TEST", (req, res) => {
 
         res.send(`API ROUTE TEST`);
+    });
+
+    // A GET route for scraping the echojs website
+    app.get("/scrape", function (req, res) {
+        // First, we grab the body of the html with request
+        request("https://news.ycombinator.com/", function(error, response, html) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(html);
+            
+            
+            // Now, we grab every h2 within an article tag, and do the following:
+            $("article h2").each(function (i, element) {
+                // Save an empty result object
+                var result = {};
+                
+
+                // Add the text and href of every link, and save them as properties of the result object
+                result.title = $(this).children("a").text();
+                result.link = $(this).children("a").attr("href");
+
+                // Create a new Article using the `result` object built from scraping
+                db.Article
+                    .create(result)
+                    .then(function (dbArticle) {
+                        // If we were able to successfully scrape and save an Article, send a message to the client
+                        res.send("Scrape Complete");
+                    })
+                    .catch(function (err) {
+                        // If an error occurred, send it to the client
+                        res.json(err);
+                    });
+            });
+        });
     });
 
 
