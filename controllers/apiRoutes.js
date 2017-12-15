229a30c288faa1270c55c79a38348c56ac49c0fe
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-15 02:18:08 -0500

    added post route for notes.

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -68,7 +68,7 @@ module.exports = (app) => {
     });
 
     // Route for grabbing a specific Article by id, populate it with it's note
-    app.get("/api/articles/:id", (req, res) =>{
+    app.get("/api/articles/:id", (req, res) => {
         // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
         db.Article.findOne({
                 _id: req.params.id
@@ -83,6 +83,34 @@ module.exports = (app) => {
                 // If an error occurred, send it to the client
                 res.json(err);
             });
+        console.log(`We grabbed the individual article for you!`);
     });
-    console.log(`We grabbed the individual article for you!`);
+
+
+    // Route for saving/updating an Article's associated Note
+    app.post("/api/articles/:id", function (req, res) {
+        // Create a new note and pass the req.body to the entry
+        db.Note
+            .create(req.body)
+            .then(function (dbNote) {
+                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+                return db.Article.findOneAndUpdate({
+                    _id: req.params.id
+                }, {
+                    note: dbNote._id
+                }, {
+                    new: true
+                });
+            })
+            .then(function (dbArticle) {
+                // If we were able to successfully update an Article, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
 };
