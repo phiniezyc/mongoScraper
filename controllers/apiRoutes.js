afb11c11072d0d3e5a1cad2c74280d472a41f132
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-15 01:15:23 -0500

    added in individual article route.  Works correctly

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -49,21 +49,40 @@ module.exports = (app) => {
             //console.log(`Here is what we grabbed ${JSON.stringify(result)}`);
         });
 
-        
+
 
     });
 
     app.get("/api/articles", (req, res) => {
         // Grab every document in the Articles collection
         db.Article.find({})
-            .then((dbArticle) =>{
+            .then((dbArticle) => {
                 // If we were able to successfully find Articles, send them back to the client
                 res.json(dbArticle);
             })
-            .catch((err) =>{
+            .catch((err) => {
                 // If an error occurred, send it to the client
                 res.json(err);
             });
         console.log("we have the articles from database");
     });
+
+    // Route for grabbing a specific Article by id, populate it with it's note
+    app.get("/api/articles/:id", (req, res) =>{
+        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+        db.Article.findOne({
+                _id: req.params.id
+            })
+            // ..and populate all of the notes associated with it
+            //.populate("note")
+            .then(function (dbArticle) {
+                // If we were able to successfully find an Article with the given id, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
+    console.log(`We grabbed the individual article for you!`);
 };
