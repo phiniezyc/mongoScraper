6da46506fd91d5442fcc241414cc8032277cce6b
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-15 20:35:14 -0500

    grabbed data and sent to page to display. Works.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,14 @@
 console.log("js file connected");
+
+// Grab the articles as a json
+$.getJSON("/api/articles", function (data) {
+    // For each one
+    data.forEach((data) => {
+        // Display the apropos information on the page
+        $("#articles").append("<p data-id='" + data._id + "'>" + data.title + "<br />" + data.link + "</p>");
+        console.log(data);
+
+    });
+
+
+});
