85b332b7c03c12bc24cd94a2329ae15c18b7408c
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-15 20:38:17 -0500

    changed for each method name. No functionality change.  WORKING.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,12 @@
 console.log("js file connected");
 
 // Grab the articles as a json
-$.getJSON("/api/articles", function (data) {
+$.getJSON("/api/articles", (articles) => {
     // For each one
-    data.forEach((data) => {
+    articles.forEach((article) => {
         // Display the apropos information on the page
-        $("#articles").append("<p data-id='" + data._id + "'>" + data.title + "<br />" + data.link + "</p>");
-        console.log(data);
+        $("#articles").append("<p data-id='" + article._id + "'>" + article.title + "<br />" + article.link + "</p>");
+        console.log(article);
 
     });
 
